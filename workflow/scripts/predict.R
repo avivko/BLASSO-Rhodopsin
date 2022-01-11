5779c7bd887a06ab6df150c738d57da8405d3aa7
alephreish <alephreish@gmail.com>
2022-01-11 17:51:20 +0200

    Add optional config["targets"], cleanup in predict.R and train.R

--- workflow/scripts/predict.R
+++ workflow/scripts/predict.R
@@ -2,15 +2,19 @@
 library(seqinr)
 library(pracma)
 library(monomvn)
+
 source("workflow/scripts/seqBlasso.R")
 
-mafft_file <- as.character(snakemake@input["mafft"])
-blasso_file <- as.character(snakemake@input["blasso"])
-pos_file   <- as.character(snakemake@input["positions"])
-amino_feature_file <- as.character(snakemake@input["amino_feature_file"])
+input <- snakemake@input
+
+mafft_file  <- input$mafft
+blasso_file <- input$blasso
+pos_file    <- input$positions
+amino_feature_file <- input$amino_feature_file
 output_file <- as.character(snakemake@output)
 
 position <- as.matrix(read.table(pos_file))
+
 # blasso object
 load(blasso_file)
 train_range <- 1:length(blasso$sample_names)
@@ -36,9 +40,15 @@ X <- do.call("cbind", lapply(1:ncol(aa_feat), function(i) {
 for (i in 1:nrow(X)) {
 	X[i,] <- c(t(matrix(X[i,], ncol(aseq_sub), ncol(aa_feat))))
 }
-wavelength <- cbind(1,X) %*% blasso$beta_mean
+wavelength <- cbind(1,X) %*% blasso$beta_sample
+wl_mean <- apply(wavelength, 1, mean)
+wl_sd <- apply(wavelength, 1, sd)
 
 ###############################################
 
-pred_wavelen_sample <- data.frame(id = sample_names[-train_range], wavelength = wavelength[-train_range])
+pred_wavelen_sample <- data.frame(
+	id = sample_names[-train_range],
+	wl_mean = wl_mean[-train_range],
+	wl_sd = wl_sd[-train_range]
+)
 write.table(pred_wavelen_sample, file = output_file, quote = F, row.names = F, sep = "\t")
--- workflow/scripts/train.R
+++ workflow/scripts/train.R
@@ -5,19 +5,20 @@ library(monomvn)
 
 source("workflow/scripts/seqBlasso.R")
 
-wavelength <- as.character(snakemake@input["wavelength"])
-positions  <- as.character(snakemake@input["positions"])
-sequences  <- as.character(snakemake@input["sequences"])
-zero       <- as.character(snakemake@input["zero"])
-amino_acid_feature <- as.character(snakemake@input["amino_acid_feature"])
+input <- snakemake@input
 
-n_sample <- as.numeric(snakemake@params["n_sample"])
-dim_prescreening <- as.numeric(snakemake@params["dim_prescreening"])
+wavelength <- input$wavelength
+positions  <- input$positions
+sequences  <- input$sequences
+zero       <- input$zero
+amino_acid_feature <- input$amino_acid_feature
+output <- as.character(snakemake@output)
+
+n_sample <- as.numeric(snakemake@params$n_sample)
+dim_prescreening <- as.numeric(snakemake@params$dim_prescreening)
 if (dim_prescreening < 0) {
 	dim_prescreening <- Inf
 }
-
-output <- as.character(snakemake@output)
 
 blasso <- train_blasso(sequences,
 	wavelength,
@@ -28,4 +29,4 @@ blasso <- train_blasso(sequences,
 	dim_prescreening = dim_prescreening
 )
 
-save(result, file = output)
+save(blasso, file = output)
